7768cdc60c889dc30914b6c7564fb934f5a85ea6
Oleksandr Kostenko <sanyokkua@gmail.com>
2026-01-03 19:43:48 +0100

    Re-implemented prompt service

---
+++ internal/prompts/prompts.go
@@ -0,0 +1,30 @@
+package prompts
+
+type Prompt struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Type     string `json:"type"`
+	Category string `json:"category"`
+	Value    string `json:"value"`
+}
+
+type PromptGroup struct {
+	GroupID      string
+	GroupName    string
+	SystemPrompt Prompt
+	Prompts      map[string]Prompt
+}
+
+type Prompts struct {
+	PromptGroups map[string]PromptGroup
+}
+
+type PromptActionRequest struct {
+	ID string `json:"id"`
+
+	InputText  string `json:"inputText"`
+	OutputText string `json:"outputText,omitempty"`
+
+	InputLanguageID  string `json:"inputLanguageId,omitempty"`
+	OutputLanguageID string `json:"outputLanguageId,omitempty"`
+}
