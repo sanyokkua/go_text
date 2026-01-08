a9151951e8f535279b8fb421af76ab6276af38c8
Oleksandr Kostenko <sanyokkua@gmail.com>
2026-01-08 19:09:16 +0100

    Removed Old UI

--- frontend/src/ui/widgets/base/LoadingSpinner.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const LoadingSpinner: React.FC = () => {
-    return <div className="loader"></div>;
-};
-
-LoadingSpinner.displayName = 'LoadingSpinner';
-export default LoadingSpinner;
