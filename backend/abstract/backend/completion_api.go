d67bf5ef2e4522316ea1249e8fcb27cde72142bb
Oleksandr Kostenko <sanyokkua@gmail.com>
2026-01-08 22:48:50 +0100

    Removed old backend code

--- backend/abstract/backend/completion_api.go
+++
@@ -1,9 +0,0 @@
-package backend
-
-import (
-	"go_text/backend/model/action"
-)
-
-type CompletionApi interface {
-	ProcessAction(action action.ActionRequest) (string, error)
-}
--- backend/abstract/backend/string_utils_api.go
+++
@@ -1,7 +0,0 @@
-package backend
-
-type StringUtilsApi interface {
-	IsBlankString(value string) bool
-	ReplaceTemplateParameter(template, value, prompt string) (string, error)
-	SanitizeReasoningBlock(llmResponse string) (string, error)
-}
