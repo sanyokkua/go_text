0fa304b2cf14c059525148b3e107e5abfa2248dc
Oleksandr Kostenko <sanyokkua@gmail.com>
2026-06-26 23:53:33 +0200

    feat(T19): add unwrap/tryUnwrap envelope helpers

---
+++ frontend/src/logic/adapter/envelope.ts
@@ -0,0 +1,21 @@
+import { apperr } from '../../../wailsjs/go/models';
+import { store } from '../store';
+import { notifyError } from '../store/notifications/slice';
+
+export function unwrap<T>(res: { data?: T; error?: apperr.WireError }): T {
+    if (res.error) {
+        store.dispatch(notifyError(res.error));
+        throw res.error;
+    }
+    return res.data as T;
+}
+
+export function tryUnwrap<T>(res: {
+    data?: T;
+    error?: apperr.WireError;
+}): { data?: T; error?: apperr.WireError } {
+    if (res.error) {
+        store.dispatch(notifyError(res.error));
+    }
+    return res;
+}
