92ba3e983525c4630f68108abc5cd26a3efeddce
Oleksandr Kostenko <sanyokkua@gmail.com>
2026-06-27 12:06:12 +0200

    feat(T21): add EditorArea with Side/Stacked layout switching

---
+++ frontend/src/ui/widgets/views/editor/EditorArea.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { selectLayout, useAppSelector } from '../../../../logic/store';
+import InputPane from './InputPane';
+import OutputPane from './OutputPane';
+import styles from './EditorArea.module.css';
+
+const EditorArea: React.FC = () => {
+    const layout = useAppSelector(selectLayout);
+
+    return (
+        <div className={layout === 'side' ? styles.side : styles.stacked}>
+            <InputPane />
+            {layout === 'side' && <div className={styles.splitter} aria-hidden="true" />}
+            <OutputPane />
+        </div>
+    );
+};
+
+EditorArea.displayName = 'EditorArea';
+export default EditorArea;
